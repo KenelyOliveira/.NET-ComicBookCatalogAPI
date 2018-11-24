c470ba932243115628d804a54a325df86c90fd39
Kenely Oliveira <kenely.oliveira@gmail.com>
2018-11-24 11:05:44 -0200

    first commit

--- Bibliotech/Controllers/LoginController.cs
+++ Bibliotech/Controllers/LoginController.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Bibliotech.Facade;
 using Bibliotech.Models;
-using Bibliotech.Repository;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Bibliotech.Controllers
 {
